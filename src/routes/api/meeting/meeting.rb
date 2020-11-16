0dc1e051c013190d1852c8d979682300e8a6c33c
yo higashida <penguin4731@gmail.com>
2020-11-17 01:43:08 +0900

    [delete] agendaphoto agendaSheetPhoto

--- src/routes/api/meeting/meeting.rb
+++ src/routes/api/meeting/meeting.rb
@@ -66,36 +66,4 @@ class MeetingRouter < Base
     content_type "image/png"
     blob
   end
-
-  # アジェンダ画像生成（タイトル(String),開始時間(UNIX時間),アジェンダのリスト(連想配列)）
-  # def agendaphoto(title, photo_start_time, agendas)
-  #   @photo_start_time = photo_start_time
-  #   agendaList = agendas.each_slice(7).to_a
-  #   # p agendaList
-  #   returnText = []
-  #   agendaList.each_with_index do |a, i|
-  #     text = { photo: agendaSheetPhoto(title, a, i + 1, agendaList.length) }
-  #     returnText = returnText.push(text)
-  #   end
-  #   returnText.to_json
-  # end
-
-  # # アジェンダ画像生成（タイトル(String),アジェンダのリスト(連想配列),何個目の画像か(Int),全体の数(int)）
-  # def agendaSheetPhoto(title, agendas, num, length)
-  #   title = "#{title.delete("\n").slice(0, 14)}…" if title.length >= 14
-  #   title += "(#{num}/#{length})"
-  #   text = ""
-  #   agendas.each do |a|
-  #     start = Time.at(@photo_start_time).strftime('%H:%M') # このアジェンダシートの開始時刻
-  #     duration = (a['duration'].to_i / 60).ceil
-  #     titleA = if a['title'].length >= 12
-  #               "#{a['title'].delete("\n").slice(0, 12)}…"
-  #             else
-  #               a['title'].delete("\n")
-  #             end
-  #     text += "#{start} #{duration.to_s}分 #{titleA}\n"
-  #     @photo_start_time += a['duration'].to_i
-  #   end
-  #   agendaWrite(title, text)
-  # end
 end
