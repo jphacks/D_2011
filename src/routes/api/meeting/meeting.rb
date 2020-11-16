e47a37d27c8a018d06f31e2546f27dfd5e1d7e21
yo higashida <penguin4731@gmail.com>
2020-11-17 01:40:54 +0900

    [add] create image api

--- src/routes/api/meeting/meeting.rb
+++ src/routes/api/meeting/meeting.rb
@@ -16,10 +16,6 @@ class MeetingRouter < Base
     ok({agenda: agendaphoto(title, start_time.to_i, JSON.parse(agendas.to_json)), url: "https://aika.lit-kansai-mentors.com/agenda/#{meeting.meeting_id}",id: meeting.meeting_id })
   end
 
-  post '/api/test' do
-    ok({status: params[:name]})
-  end
-
   # ミーティング開始
   post '/api/meeting/start' do
     zoom = ZoomClient.connect_with_number(params[:meetingid], params[:meetingpass])
@@ -49,41 +45,57 @@ class MeetingRouter < Base
 
   # アジェンダ画像を返す
   get '/api/meeting/img/:meeting_id' do
-    
-    blob = agendaWrite('ABC',params[:meeting_id])
+    meeting = Meeting.find_by(meeting_id: params[:meeting_id])
+    title = meeting.title
+    title = "#{title.delete("\n").slice(0, 14)}…" if title.length >= 14
+    agendas = Agenda.where(meeting_id: meeting.id)
+    time_text = ""
+    content_text = ""
+    p agendas
+    agendas.each_with_index do |value,i|
+      p value.duration
+      time_text += ((value.duration / 60).ceil).to_s + "分\n"
+      content_text += if value.title.length >= 12
+                        "#{value.title.delete("\n").slice(0, 12)}…\n"
+                      else
+                        value.title.delete("\n")+"\n"
+                      end
+      break if i == 6
+    end
+    blob = agendaWrite(title,time_text,content_text)
     content_type "image/png"
     blob
   end
 
   # アジェンダ画像生成（タイトル(String),開始時間(UNIX時間),アジェンダのリスト(連想配列)）
-  def agendaphoto(title, photo_start_time, agendas)
-    @photo_start_time = photo_start_time
-    agendaList = agendas.each_slice(7).to_a
-    # p agendaList
-    returnText = []
-    agendaList.each_with_index do |a, i|
-      text = { photo: agendaSheetPhoto(title, a, i + 1, agendaList.length) }
-      returnText = returnText.push(text)
-    end
-    returnText.to_json
-  end
+  # def agendaphoto(title, photo_start_time, agendas)
+  #   @photo_start_time = photo_start_time
+  #   agendaList = agendas.each_slice(7).to_a
+  #   # p agendaList
+  #   returnText = []
+  #   agendaList.each_with_index do |a, i|
+  #     text = { photo: agendaSheetPhoto(title, a, i + 1, agendaList.length) }
+  #     returnText = returnText.push(text)
+  #   end
+  #   returnText.to_json
+  # end
 
-  # アジェンダ画像生成（タイトル(String),アジェンダのリスト(連想配列),何個目の画像か(Int),全体の数(int)）
-  def agendaSheetPhoto(title, agendas, num, length)
-    title = "#{title.delete("\n").slice(0, 14)}…" if title.length >= 14
-    title += "(#{num}/#{length})"
-    text = ""
-    agendas.each do |a|
-      start = Time.at(@photo_start_time).strftime('%H:%M') # このアジェンダシートの開始時刻
-      duration = (a['duration'].to_i / 60).ceil
-      titleA = if a['title'].length >= 12
-                "#{a['title'].delete("\n").slice(0, 12)}…"
-              else
-                a['title'].delete("\n")
-              end
-      text += "#{start} #{duration.to_s}分 #{titleA}\n"
-      @photo_start_time += a['duration'].to_i
-    end
-    agendaWrite(title, text)
-  end
+  # # アジェンダ画像生成（タイトル(String),アジェンダのリスト(連想配列),何個目の画像か(Int),全体の数(int)）
+  # def agendaSheetPhoto(title, agendas, num, length)
+  #   title = "#{title.delete("\n").slice(0, 14)}…" if title.length >= 14
+  #   title += "(#{num}/#{length})"
+  #   text = ""
+  #   agendas.each do |a|
+  #     start = Time.at(@photo_start_time).strftime('%H:%M') # このアジェンダシートの開始時刻
+  #     duration = (a['duration'].to_i / 60).ceil
+  #     titleA = if a['title'].length >= 12
+  #               "#{a['title'].delete("\n").slice(0, 12)}…"
+  #             else
+  #               a['title'].delete("\n")
+  #             end
+  #     text += "#{start} #{duration.to_s}分 #{titleA}\n"
+  #     @photo_start_time += a['duration'].to_i
+  #   end
+  #   agendaWrite(title, text)
+  # end
 end
