ff28bdd3c0149d5de51160b8e0e85d491194d175
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-11-20 01:09:23 +0900

    update agenda.rb

--- src/routes/api/meeting/agenda.rb
+++ src/routes/api/meeting/agenda.rb
@@ -4,9 +4,16 @@
 class AgendaRouter < Base
   # 議題変更
   post '/api/meeting/:id/agenda/next' do
+    meeting = Meeting.find_by(meeting_id: params[:id])
     zoom = ZoomManager.instance.get(params[:id])
+    ZoomManager.instance.terminate(meeting.id)
     not_found("No such meeting: #{params[:id]}") if zoom.nil?
     File.delete("public/assets/img/tmp/#{params[:id]}.png") rescue puts $!
     zoom.changeImage(topicWrite("#{params[:title]}\n(#{params[:duration]}分)", id))
+  end
+
+  post '/api/meeting/:id/agenda/delay' do 
+    meeting = Meeting.find_by(meeting_id: params[:id])
+    ZoomManager.instance.delay(meeting.id, params[:duration])
   end
 end
