e2264a804585745ffb828789344132b2a977aa6d
yo higashida <penguin4731@gmail.com>
2020-11-20 21:41:51 +0900

    [delete] test code

--- src/routes/api/meeting/meeting.rb
+++ src/routes/api/meeting/meeting.rb
@@ -75,9 +75,7 @@ class MeetingRouter < Base
 
   # OGP画像を返す
   get '/api/ogp/:id' do
-    # meeting = Meeting.find_by(meeting_id: params[:id])
-    meeting = Meeting.last
-    print(meeting.meeting_id)
+    meeting = Meeting.find_by(meeting_id: params[:id])
     title = meeting.title
     title = "#{title.delete("\n").slice(0, 14)}…" if title.length >= 14
     time_text = meeting.start_time
