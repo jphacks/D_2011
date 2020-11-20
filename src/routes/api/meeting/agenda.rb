dea3831e66ad184c2bb40fa42bed32e7ef7a4b2a
yo higashida <penguin4731@gmail.com>
2020-11-21 00:50:27 +0900

    [add] get from db

--- src/routes/api/meeting/agenda.rb
+++ src/routes/api/meeting/agenda.rb
@@ -7,6 +7,15 @@ class AgendaRouter < Base
     zoom = ZoomManager.instance.get(params[:id])
     not_found("No such meeting: #{params[:id]}") if zoom.nil?
     File.delete("public/assets/img/tmp/#{params[:id]}.png") rescue puts $!
-    zoom.changeImage(topic_write("#{params[:title]}\n(#{params[:duration]}分)", id))
+    meeting = Meeting.find_by(meeting_id: params[:id])
+    agendas = Agenda.where(meeting_id: meeting.id)
+
+    next_agenda = agendas[meeting.agenda_now + 1]
+    if next_agenda.nil?
+      not_found("Not found next agenda.")
+    end
+
+    zoom.changeImage(topic_write("#{next_agenda.title}\n(#{(next_agenda.duration / 60).to_s}分)", id))
+    ok
   end
 end
