59ae91c129daea886ea0fbd39d22b3debf61c3bb
Ritsuki Goto <develop@mizucoffee.net>
2020-11-21 20:31:41 +0900

    [fix] image edit

--- spec/image_edit_spec.rb
+++ spec/image_edit_spec.rb
@@ -2,12 +2,12 @@
 
 RSpec.describe 'image_edit.rb' do
   describe 'アジェンダ一覧画像生成' do
-    before :all do
-      @blob = agenda_write("タイトル", "5分", "挨拶")
-      puts @blob
-    end
-
-    example('レスポンスがOKである') { expect(@blob).to be_ok }
+    before(:all) { @blob = ImageEdit.agenda_write('タイトル', '5分', '挨拶') }
+    example('画像が生成された') { expect(@blob).not_to be_empty }
   end
-  
+
+  describe 'アジェンダ一覧画像生成' do
+    before(:all) { @blob = ImageEdit.agenda_write('タイトル', '5分', '挨拶') }
+    example('画像が生成された') { expect(@blob).not_to be_empty }
+  end
 end
--- src/image_edit.rb
+++ src/image_edit.rb
@@ -3,76 +3,76 @@
 require 'mini_magick'
 require 'securerandom'
 
-before do
+class ImageEdit
   @assets_path = 'public/assets'
   @img_path = "#{@assets_path}/img"
   @font = 'public/assets/fonts/unifont-11.0.01.ttf'
   @color = 'white'
   @topic_indention_count = 10
   @topic_row_limit = 8
-end
 
-# ----------------------------``
-# 画像の書き出し
-# ----------------------------
-# アジェンダ用の画像生成(アプリへの送信用にJSONに変換)（タイトル , 内容）
-def agenda_write(title, time_text, content_text)
-  @image = MiniMagick::Image.open("#{@img_path}/bg.jpg")
-
-  configuration(title, 'North', 80, '0,50') # 画像の生成（タイトル）
-  configuration(time_text, 'NorthWest', 60, '100,200') # 画像の生成（時間）
-  configuration(content_text, 'NorthWest', 60, '300,200') # 画像の生成（内容）
-
-  @image.to_blob
-end
-
-# Zoomの仮想カメラ用の画像生成(テキスト , イメージ名)
-def topic_write(print_text, image_name)
-  # 画像の生成
-  @image = MiniMagick::Image.open("#{@img_path}/bg.jpg")
-  configuration(print_text, 'center', 100, '0,0')
+  # ----------------------------``
   # 画像の書き出し
-  image_name += '.png'
-  @image.write image_name
+  # ----------------------------
+  # アジェンダ用の画像生成(アプリへの送信用にJSONに変換)（タイトル , 内容）
+  def self.agenda_write(title, time_text, content_text)
+    @image = MiniMagick::Image.open("#{@img_path}/bg.jpg")
 
-  path = "#{@img_path}/tmp/#{image_name}"
-  FileUtils.mv(image_name, path)
-  path
-end
+    configuration(title, 'North', 80, '0,50') # 画像の生成（タイトル）
+    configuration(time_text, 'NorthWest', 60, '100,200') # 画像の生成（時間）
+    configuration(content_text, 'NorthWest', 60, '300,200') # 画像の生成（内容）
 
-# OGP用の画像生成
-def ogpWrite(title,time_text)
-  @image = MiniMagick::Image.open("public/assets/img/ogp_bg.png")
-  configuration(title,'center',80,'0,-30')
-  configuration(time_text,'center',40,'0,40')
-  return @image.to_blob
-end
-
-private
-
-# ----------------------------
-# 文字オーバーしない用に調整して文字列を返却
-# ----------------------------
-# Zoomの仮想カメラ用の画像生成
-def topic_prepare_text(print_text)
-  print_text.scan(/.{1,#{@topic_indention_count}}/)[0...@topic_row_limit].join("\n")
-end
-
-# ----------------------------
-# 共通処理
-# ----------------------------
-# 設定関連を代入 & 文字列を合成(位置は任意のもの)
-def configuration(text, gravity, pointsize, text_position)
-  @image.combine_options do |config|
-    config.fill @color
-    config.font @font
-    config.gravity gravity
-    config.pointsize pointsize.to_i
-    config.draw "text #{text_position} '#{text}'"
+    @image.to_blob
   end
-end
 
-# uniqなファイル名を返却
-def uniq_file_name
-  "#{SecureRandom.hex}.png"
+  # Zoomの仮想カメラ用の画像生成(テキスト , イメージ名)
+  def self.topic_write(print_text, image_name)
+    # 画像の生成
+    @image = MiniMagick::Image.open("#{@img_path}/bg.jpg")
+    configuration(print_text, 'center', 100, '0,0')
+    # 画像の書き出し
+    image_name += '.png'
+    @image.write image_name
+
+    path = "#{@img_path}/tmp/#{image_name}"
+    FileUtils.mv(image_name, path)
+    path
+  end
+
+  # OGP用の画像生成
+  def self.ogpWrite(title,time_text)
+    @image = MiniMagick::Image.open("public/assets/img/ogp_bg.png")
+    configuration(title,'center',80,'0,-30')
+    configuration(time_text,'center',40,'0,40')
+    return @image.to_blob
+  end
+
+  # ----------------------------
+  # 文字オーバーしない用に調整して文字列を返却
+  # ----------------------------
+  # Zoomの仮想カメラ用の画像生成
+  def self.topic_prepare_text(print_text)
+    print_text.scan(/.{1,#{@topic_indention_count}}/)[0...@topic_row_limit].join("\n")
+  end
+
+  # ----------------------------
+  # 共通処理
+  # ----------------------------
+  # 設定関連を代入 & 文字列を合成(位置は任意のもの)
+  def self.configuration(text, gravity, pointsize, text_position)
+    @image.combine_options do |config|
+      config.fill @color
+      config.font @font
+      config.gravity gravity
+      config.pointsize pointsize.to_i
+      config.draw "text #{text_position} '#{text}'"
+    end
+  end
+
+  # uniqなファイル名を返却
+  def self.uniq_file_name
+    "#{SecureRandom.hex}.png"
+  end
+
+  private_class_method :topic_prepare_text, :configuration, :uniq_file_name
 end
