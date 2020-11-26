cd96ba79722dc5b655fced9247a6719764cd70d6
yo higashida <penguin4731@gmail.com>
2020-11-26 19:33:50 +0900

    [fix] meeting.last

--- src/routes/root.rb
+++ src/routes/root.rb
@@ -12,8 +12,8 @@ class RootRouter < Base
 
   # アジェンダページの表示
   get '/agenda/:id' do
-    @meeting = Meeting.last
-    # @meeting = Meeting.find_by(meeting_id: params[:id])
+    # @meeting = Meeting.last
+    @meeting = Meeting.find_by(meeting_id: params[:id])
     @agenda_times = []
     agenda_starting_time = @meeting.start_time
     @meeting.agendas.each do |agenda|
