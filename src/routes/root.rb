d0f77cacb799e8bb83c220d4cef9f366f87749ce
mikik0 <usapon_love1222@yahoo.co.jp>
2020-11-28 04:43:03 +0900

    編集機能

--- src/routes/root.rb
+++ src/routes/root.rb
@@ -17,4 +17,21 @@ class RootRouter < Base
     @start_time = Time.at(@meeting.start_time).strftime('%Y年%m月%d日 %H:%M')
     erb :invite
   end
+
+  #アジェンダの編集ページ
+  get '/agenda/:id/edit' do
+    @meeting = Meeting.find_by(meeting_id: params[:id])
+    @start_time = Time.at(@meeting.start_time).strftime('%Y年%m月%d日 %H:%M')
+    erb :edit
+  end
+
+  post '/agenda/:id/edit' do
+    @meeting = Meeting.find_by(meeting_id: params[:id])
+    @start_time = Time.at(@meeting.start_time).strftime('%Y年%m月%d日 %H:%M')
+
+    agenda.duration = params[:duration]
+    agenda.title = params[:title]
+    agenda.save
+    redirect "/agenda/:id"
+  end
 end
